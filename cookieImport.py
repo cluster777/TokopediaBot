03ab372882e3799e5f10dc44548c0eb821ed3abc
cluster777 <davidlaksmanasan@gmail.com>
2021-08-31 17:28:47 +0700

    masuk menu chat

--- cookieImport.py
+++ cookieImport.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 import json
+import time
 driver=webdriver.Chrome()
 with open('cookieTest2.txt','r', newline='')as tmp:
     cookies = json.load(tmp)
@@ -11,6 +12,13 @@ for curcookie in cookies:
     driver.add_cookie(curcookie)
 driver.refresh()
 #go to messaging
+time.sleep(5)
+try:
+    driver.find_element_by_xpath("//button[@aria-label='Tutup tampilan modal']").click()
+except:
+    print("nothing wrong")
+driver.find_element_by_xpath("//div[@data-testid='btnHeaderInbox']").click()
+driver.find_element_by_xpath("//a[@href='/chat']").click()
 #while check 
 #if there is indicator do reply
 #sleep n time
