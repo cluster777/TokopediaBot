d3baf396732e9df2fd43616d2ea042789deb74c8
cluster777 <davidlaksmanasan@gmail.com>
2021-09-16 18:00:06 +0700

    add function send message on scrapper

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup as bs
 import re
+import telebot
+from telebot import types
 
+import json
 def get_nChat(target,n):
     soup=bs(target,'html.parser')
     chatBoxAll = soup.find('div', {'class': 'css-1r71t1x'})
@@ -9,5 +12,13 @@ def get_nChat(target,n):
     chatAll=chatAll.findChildren("div" , recursive=False)
     return chatAll[-n:]
 
-with open('../tokopedia bot html/Chat _ Tokopedia.html') as f:
-    get_nChat(f.read(),2)
+def sendChat(message):
+    setting={}
+    with open('./setting.json')as f:
+        setting=json.load(f)
+    API_TOKEN = setting['Token']
+    bot = telebot.TeleBot(API_TOKEN)
+    # if want to send direct message use this python
+    k = types.InlineKeyboardMarkup()
+    k.add(types.InlineKeyboardButton("reply", callback_data="reply"))
+    bot.send_message(setting["ChatId"], "the content........ ok insert meme here", reply_markup=k)
