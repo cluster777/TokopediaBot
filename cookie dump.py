a21a7e903f0c0ada8d51f00f15ba5d2203dfd3e6
cluster777 <davidlaksmanasan@gmail.com>
2021-09-16 14:23:53 +0700

    found the structure of chat

--- cookie dump.py
+++ cookie dump.py
@@ -1,15 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import time
 import json
-driver=webdriver.Chrome()
+chrome_options = Options()
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36' 
+chrome_options.add_argument("--disable-logging")  
+chrome_options.add_argument('user-agent={0}'.format(user_agent))
+chrome_options.add_argument('window-size=1920x1080');
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.tokopedia.com/")
 while True:
-    tmp=input("input \"finish\" after finishing a login")
+    tmp=input("input \"finish\" after finishing a login >>> ")
     if(tmp=="fin" or tmp=="finish"):
         break
 print(driver.get_cookies())
-with open('cookieTest2.txt','w', newline='')as tmp:
+
+with open('cookieTestmm.txt','w', newline='')as tmp:
     json.dump(driver.get_cookies(), tmp, indent = 6)
 driver.close()
--- cookieImport.py
+++ cookieImport.py
@@ -9,12 +9,12 @@ import json
 import time
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'    
 chrome_options.add_argument('user-agent={0}'.format(user_agent))
 chrome_options.add_argument('window-size=1920x1080');
 driver = webdriver.Chrome(options=chrome_options)
-with open('cookieTest2.txt','r', newline='')as tmp:
+with open('cookieTestpp.txt','r', newline='')as tmp:
     cookies = json.load(tmp)
 driver.get("https://www.tokopedia.com/")
 for curcookie in cookies:
