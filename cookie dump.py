f7beda7366ebb84f6b24c6e46eb9c33444ac940c
cluster777 <davidlaksmanasan@gmail.com>
2021-10-25 11:19:19 +0700

    oops double import

--- cookie dump.py
+++ cookie dump.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
