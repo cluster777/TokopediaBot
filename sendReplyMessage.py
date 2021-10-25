f56d67ba5a5d7f5c0800e0c852ae30c075f6394d
cluster777 <davidlaksmanasan@gmail.com>
2021-10-25 11:20:29 +0700

    add 1 more fix to sendReplyMessage same as before

--- sendReplyMessage.py
+++ sendReplyMessage.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -16,7 +17,7 @@ def sendReplyMessage(name,message):
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'    
     chrome_options.add_argument('user-agent={0}'.format(user_agent))
     chrome_options.add_argument('window-size=1920x1080');
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
     with open('cookieTestmm.txt','r', newline='')as tmp:
         cookies = json.load(tmp)
     while True:
