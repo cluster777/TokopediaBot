ee075bc037d6e9ba1333fbbefda98a0b5ca286b4
cluster777 <davidlaksmanasan@gmail.com>
2021-11-06 15:18:13 +0700

    add GUI for cookieDump

--- cookie dump.py
+++ cookie dump.py
@@ -10,15 +10,32 @@ user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Ge
 chrome_options.add_argument("--disable-logging")  
 chrome_options.add_argument('user-agent={0}'.format(user_agent))
 chrome_options.add_argument('window-size=1920x1080');
-
+from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QMainWindow, QApplication
+from PyQt5 import uic
+import sys
+ 
 driver = webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
 driver.get("https://www.tokopedia.com/")
-while True:
-    tmp=input("input \"finish\" after finishing a login >>> ")
-    if(tmp=="fin" or tmp=="finish"):
-        break
-print(driver.get_cookies())
 
-with open('cookieTestmm.txt','w', newline='')as tmp:
-    json.dump(driver.get_cookies(), tmp, indent = 6)
-driver.close()
+class UI(QDialog):
+    def __init__(self):
+        super(UI, self).__init__()
+        uic.loadUi("cookieDump.ui", self)
+        self.button = self.findChild(QDialogButtonBox, "finishButton")
+        self.accepted.connect(self.acc)
+        self.rejected.connect(self.rej)
+        self.show()
+    def acc(self):
+        print("get Cookie")
+        print(driver.get_cookies())
+        with open('cookieTestmm.txt','w', newline='')as tmp:
+            json.dump(driver.get_cookies(), tmp, indent = 6)
+
+        driver.close()
+    def rej(self):
+        print("exit")
+        driver.close()
+
+app = QApplication(sys.argv)
+window = UI()
+app.exec_()
